48e7bf45d027150ecb2dbaff35ab4681bc709295
Taylor Gregory <taylor.gregory@connect.qut.edu.au>
2019-04-26 12:35:44 +0200

    edits to patient class

--- hospital/src/main/java/hospital/Patient.java
+++ hospital/src/main/java/hospital/Patient.java
@@ -12,7 +12,8 @@ public class Patient extends Person {
 	private int queueNumber;
 
 	// Constructors
-	public Patient() {}
+	// constructor if the clerk wants to have a patient number automatically assigned
+	// need to remove the patient number in the final implementation of this. this is just kept here for now for the other test cases
 	public Patient(String FirstName, String LastName, Department dep, String dob, String address, String phone, boolean living, int PatientNo, String national, Bed BedID, int queue) {
 		this.firstName = FirstName;
 		this.lastName = LastName;
@@ -22,6 +23,7 @@ public class Patient extends Person {
 		this.phoneNumber = phone;
 		this.alive = living;
 		this.patientNumber = PatientNo;
+		// the above should be swapped to a scenario where we need to add to the previous highest patient number
 		this.nationality = national;
 		this.bed = BedID;
 		this.queueNumber = queue;
@@ -31,49 +33,49 @@ public class Patient extends Person {
 	public String getBirthday() {
 		return this.birthday;
 	}
-	public void setBirthday() {
+	public void setBirthday(String newBirthday) {
 		this.birthday = newBirthday;
 	}
 	public String getAddress(){
 		return this.homeAddress;
 	}
-	public void setAddress() {
+	public void setAddress(String newAddress) {
 		this.homeAddress = newAddress;
 	}
 	public String getPhoneNumber(){
 		return this.phoneNumber;
 	}
-	public void setPhoneNumber(){
+	public void setPhoneNumber(String newPhoneNumber){
 		this.phoneNumber = newPhoneNumber;
 	}
 	public Boolean IsAlive(){
 		return this.alive;
 	}
-	public void setAlive(){
+	public void setAlive(boolean newAlive){
 		this.alive = newAlive;
 	}
 	public int getPatientNumber(){
 		return this.patientNumber;
 	}
-	public void setPatientNumber(){
+	public void setPatientNumber(int newPatientNumber){
 		this.patientNumber = newPatientNumber;
 	}
 	public String getNationality(){
 		return this.nationality;
 	}
-	public void setNationality(){
+	public void setNationality(String newNationality){
 		this.nationality = newNationality;
 	}
 	public Bed getBed(){
 		return this.bed;
 	}
-	public void setBed(){
+	public void setBed(Bed newBed){
 		this.bed = newBed;
 	}
 	public int getQueueNumber(){
 		return this.queueNumber;
 	}
-	public void setQueueNumber(){
+	public void setQueueNumber(int newQueueNumber){
 		this.queueNumber = newQueueNumber;
 	}
 }
