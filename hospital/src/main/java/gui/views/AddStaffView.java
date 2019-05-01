2d9df745ce59f24b780776d9cd5dd4deda0e81a1
Taylor Gregory <taylor.gregory@connect.qut.edu.au>
2019-05-01 19:33:13 +0200

    adding exclamations to modify button usage

--- hospital/src/main/java/gui/views/AddStaffView.java
+++ hospital/src/main/java/gui/views/AddStaffView.java
@@ -35,24 +35,51 @@ private static final long serialVersionUID = 8981053836072595592L;
 	private void initGUI() {
 		
 		setResizable(false);
-		setTitle("Login");
+		setTitle("Add Staff Member");
 		setLayout(new GridBagLayout());
 		
 		txtNames.add("First Name");
 		txtNames.add("Last Name");
 		txtNames.add("Department Name");
-		txtNames.add("Jobrole");
+		txtNames.add("Job Role");
 		
 		for ( int i=0; i<txtNames.size();++i) {
 			this.txtEntries.add(new JTextField(20));
 			add(new JLabel(txtNames.get(i)+":"), GridBagLayoutUtils.constraint(0, i, 5));
 			add(txtEntries.get(txtEntries.size()-1), GridBagLayoutUtils.constraint(1, i, 5));
-			
+			add(new JLabel("<html><font color=red>!</font></html>"), GridBagLayoutUtils.constraint(2, i, 5));	
 		}
+		
+		// if fn is empty
+			// exclaim is visible
+			// hover over exclaim says 'first name field is empty' --> append
+		
+		// if ln is empty
+			// exclaim is visible
+			// hover over exclaim says 'first name field is empty'
+		
+		// if dep is empty
+			// exclaim is visible
+			// hover over exclaim says 'dep field is empty'
+		
+		// if dep is invalid
+			// exclaim is visible
+			// hover over exclaim says 'dep field is invalid'
+		
+		// if job is empty
+			// exclaim is visible
+			// hover over exclaim says 'job field is empty'
+		
+		// if fn is empty
+			// exclaim is visible
+			// hover over exclaim says 'first name field is empty'
+		
+		// if (i1 && i2 && i3 && i4) {
+			// 
 		
 		
 		//add a button.
-		btnSave = new JButton("Save");
+		btnSave = new JButton("Add");
 		btnSave.setBounds(50, 150, 100, 30);
 		add(btnSave,GridBagLayoutUtils.constraint(1, txtEntries.size(), 5));
 		btnSave.addActionListener(new ActionListener() {
