1037538b3edf8347d7692a1b9cc44ff8966e39ba
2017029334 <ghio12@naver.com>
2019-05-01 14:38:56 +0200

    addPatient() in System

--- hospital/src/main/java/hospital/System.java
+++ hospital/src/main/java/hospital/System.java
@@ -11,11 +11,11 @@ public class System {
 	public static void registerPatient(Hospital data, String FirstName, String LastName, String depName, String dob, String address, String phone, boolean living, String national){
 		
 		Department  dep = Finder.findDepartment(depName, data.getDepartment());
-		Patient highest = (Patient) data.getPatient().get(data.getPatient().size()-1).;
+		Patient highest = (Patient) data.getPatient().get(data.getPatient().size()-1);
 		int num = highest.getPatientNumber()+1;
 		
 		Patient pat = new Patient(FirstName, LastName, dep, dob, address, phone, living, num, national, null,0);
-		data.addPatient(pat);
+		data.addPatient(pat, dep);
 	}
 	
 	
