26e18a1dab9ff8b8d55c53541ab7524a40efe52e
Taylor Gregory <taylor.gregory@connect.qut.edu.au>
2019-05-01 19:36:39 +0200

    trying to implement admit patient function

--- hospital/src/main/java/hospital/Department.java
+++ hospital/src/main/java/hospital/Department.java
@@ -23,8 +23,9 @@ public class Department {
 		this.name = Name;
 	}
 
+	// 
 	public boolean admitPatient(Patient newPatient) {
-		return this.admittedPatients.add(newPatient);
+		return this.admittedPatients.add(newPatient);	
 	}
 
 	public boolean dischargePatient(Patient patient) {
