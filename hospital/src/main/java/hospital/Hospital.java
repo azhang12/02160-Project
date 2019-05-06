c7d3ddb342b3d428342379139f33b952379457f5
ga97leh <kilian.speiser@tum.de>
2019-05-06 21:12:26 +0200

    data update

--- hospital/src/main/java/hospital/Hospital.java
+++ hospital/src/main/java/hospital/Hospital.java
@@ -32,40 +32,7 @@ public class Hospital {
 		this.patientList = p;
 	}
 
-	// Staff
-	public void addStaff(Staff newStaff) {
-		this.staffList.add(newStaff);
-	}
-
-	public void removeStaff(Staff rmStaff) {
-		this.staffList.remove(rmStaff);
-	}
-
-	// Patient
-	public void addPatient(Patient newPatient) {
-		this.patientList.add(newPatient);
-		
-	}
 	
-	
-
-	public void removePatient(Patient rmPatient, Department department) {
-		this.patientList.remove(rmPatient);
-		department.dischargePatient(rmPatient);
-	}
-
-	public void movePatient(Patient patient, Bed newBed) {
-		patient.setBed(newBed);
-	}
-
-	// Department
-	public void addDepartment(Department newDepartment) {
-		this.departmentList.add(newDepartment);
-	}
-
-	public void removeDepartment(Department rmDepartment) {
-		this.departmentList.remove(rmDepartment);
-	}
 
 	// getters
 	public  ArrayList<Staff> getStaff() {
